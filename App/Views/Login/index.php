d68cf835547cdb2a5ca131e3b4acc17807acd3c4
Fernando <125415226+JackLuengo@users.noreply.github.com>
2024-03-14 23:26:00 -0300

    login

--- App/Views/Login/index.php
+++ App/Views/Login/index.php
@@ -1,19 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./Public/css/estiloLogin.css" rel="stylesheet">
     <title>Login</title>
 </head>
+
 <body>
-    <div class="contenedorLogin">
-        <div class="contenedorForm">
-            <form method="post" action="#">
-                <input type="text" name="usuario" id="usuario" placeholder="Ingrese el usuario">
-                <input type="password" name="clave" id="clave" placeholder="Ingrese la contraseña"> 
-                <input type="submit" value="Ingresar">
-            </form>
+    <div class="contenedor">
+        <div class="mitad right">
+            <img src="./Public/img/Unete.png" alt="">
+        </div>
+        <div class="mitad left">
+            <div class="logo">
+                <img src="./Public/img/Logo.png" alt="Logo_los_leones">
+            </div>
+            <div class="contForm">
+                <h2>Ingrese sus datos:</h2>
+                <form method="post" action="">
+                    <input type="text" name="usuario" id="usuario" placeholder="Ingrese usuario">
+                    <input type="password" name="password" id="password" placeholder="Ingrese su password">
+                    <input type="submit" value="Ingresar">
+                </form>
+            </div>
         </div>
     </div>
+  
 </body>
+
 </html>
