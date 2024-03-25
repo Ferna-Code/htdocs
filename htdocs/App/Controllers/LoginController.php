785f9e12b12bef12f990bede28216f3e3b04da9d
Dian <diandrapalaciosgutierrez@gmail.com>
2024-03-25 20:48:46 -0300

    maqueta vistas

---
+++ htdocs/App/Controllers/LoginController.php
@@ -0,0 +1,8 @@
+<?php
+
+class LoginController{
+
+    public function index(){
+        require VIEWS_PATH . 'Login/index.php';
+    }
+}
---
+++ htdocs/App/Controllers/PerfilController.php
@@ -0,0 +1,8 @@
+<?php
+
+class PerfilController{
+
+    public function index(){
+        include VIEWS_PATH . 'Perfil/index.php';
+    }
+}
