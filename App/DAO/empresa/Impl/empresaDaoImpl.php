92bf2e8370f06a4992c67c399486e03cc460be2f
Fernando Reyes <fernaluengo@gmail.com>
2024-06-22 00:44:35 -0400

    actualizacion oferta

--- App/DAO/empresa/Impl/empresaDaoImpl.php
+++ App/DAO/empresa/Impl/empresaDaoImpl.php
@@ -15,36 +15,46 @@ class EmpresaDaoImpl implements EmpresaDao
 
 
 
-    public function insertData($tipoTrabajo, $categoria, $cargo, $nombreEmpresa, $rutEmpresa, $email, $descripcionCargo, $rangoSalarial, $activate)
-{
-    // Query SQL con marcadores de posición (?)
-    $validateQuery = "INSERT INTO ofertas (tipoOferta, idcategoria, cargo, nombreEmpresa, rutempresa, correocontacto, descripcion, rangosalarial, fechacreacion, fechaeliminacion, activate) 
-                      VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), DATE_ADD(NOW(), INTERVAL 3 MONTH), ?)";
-    
-    // Preparar la consulta
-    $stmt = mysqli_prepare($this->db->conec(), $validateQuery);
-    if ($stmt === false) {
-        // Manejar el error si la preparación falla
-        die('Error al preparar la consulta: ' . mysqli_error($this->db->conec()));
+    public function insertData($tipoTrabajo, $categoria, $cargo, $nombreEmpresa, $rutEmpresa, $email, $descripcionCargo, $rangoSalarial, $activate, $verificar)
+    {
+        // Verificar si se debe proceder con la inserción
+        if ($verificar == 1) {
+
+            $validateQuery = "INSERT INTO ofertas (tipoOferta, idcategoria, cargo, nombreEmpresa, rutempresa, correocontacto, descripcion, rangosalarial, fechacreacion, fechaeliminacion, activate) 
+                          VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), DATE_ADD(NOW(), INTERVAL 3 MONTH), ?)";
+
+
+            $stmt = mysqli_prepare($this->db->conec(), $validateQuery);
+            if ($stmt === false) {
+
+                die('Error al preparar la consulta: ' . mysqli_error($this->db->conec()));
+            }
+
+
+            mysqli_stmt_bind_param($stmt, "sssssssss", $tipoTrabajo, $categoria, $cargo, $nombreEmpresa, $rutEmpresa, $email, $descripcionCargo, $rangoSalarial, $activate);
+
+
+            $result = mysqli_stmt_execute($stmt);
+
+            if ($result === false) {
+
+                die('Error al ejecutar la consulta: ' . mysqli_stmt_error($stmt));
+            }
+
+
+            mysqli_stmt_close($stmt);
+
+
+            return $result;
+        } else {
+            echo "<script>alert('No se puede publicar la oferta: contiene palabras no permitidas.');</script>";
+            return false;
+        }
     }
-    
-    // Vincular los parámetros a la consulta preparada
-    mysqli_stmt_bind_param($stmt, "sssssssss", $tipoTrabajo, $categoria, $cargo, $nombreEmpresa, $rutEmpresa, $email, $descripcionCargo, $rangoSalarial, $activate);
-    
-    // Ejecutar la consulta
-    $result = mysqli_stmt_execute($stmt);
-    if ($result === false) {
-        // Manejar el error si la ejecución falla
-        die('Error al ejecutar la consulta: ' . mysqli_stmt_error($stmt));
-    }
-    
-    // Devolver el resultado de la ejecución (true si fue exitosa, false si no)
-    return $result;
-}
+
 
     public function checkData(Empresa_model $admin)
     {
-
         $tipoTrabajo = $admin->getTipoTrabajo();
         $categoria = $admin->getCatTrabajo();
         $cargo = $admin->getCargo();
@@ -59,26 +69,30 @@ class EmpresaDaoImpl implements EmpresaDao
         mysqli_stmt_execute($stmt);
         $result = mysqli_stmt_get_result($stmt);
 
-        $activate = 1;
-
         $palabras = [];
         while ($row = mysqli_fetch_assoc($result)) {
             $palabras[] = $row['palabra'];
         }
 
+        //separa el contenido del mensaje guardandolo en un array
         $palabrasEnDescripcion = preg_split('/\s+/', $descripcionCargo);
 
+        $variables = array_merge(
+            [$tipoTrabajo, $categoria, $cargo, $nombreEmpresa, $rutEmpresa, $email, $descripcionCargo, $rangoSalarial],
+            $palabrasEnDescripcion
+        );
 
-
-        $variables = array_merge([$tipoTrabajo, $categoria, $cargo, $nombreEmpresa, $rutEmpresa, $email, $descripcionCargo, $rangoSalarial], $palabrasEnDescripcion);
         $activate = 1;
+        $verificar = 1;
+        //Compara si existe alguna palabra prohibida en el contenido, si es que llega a existir verificar cambia a 0 y impide la inserción
         foreach ($variables as $variable) {
             if (in_array($variable, $palabras)) {
-                $activate = 0;
+                $verificar = 0;
                 break;
             }
         }
-        $insertResult = $this->insertData($tipoTrabajo, $categoria, $cargo, $nombreEmpresa, $rutEmpresa, $email, $descripcionCargo, $rangoSalarial, $activate);
+
+        $insertResult = $this->insertData($tipoTrabajo, $categoria, $cargo, $nombreEmpresa, $rutEmpresa, $email, $descripcionCargo, $rangoSalarial, $activate, $verificar);
         return $insertResult;
     }
 }
